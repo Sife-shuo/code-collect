d3d1ac31aa9d9bdd97ef4e9b06c4645203440325
Sife-shuo <88375951+Sife-shuo@users.noreply.github.com>
2023-02-28 22:15:04 +0800

    Create nobook.js

---
+++ website/nobook.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         NOOBOOK Chemistry
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       Sifer
+// @match        https://*.nobook.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=tampermonkey.net
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var v=fetch;
+    window.fetch=function(a,b){
+        console.log(arguments);
+        if(a=="libs/chem/allEquipmentMessage.json?subjectId=2"){return v("https://sife.ml/study/nobook-che.json")}
+        if(a=="assets/get_scene_tool.json?subjectId=1"){return v("https://sife.ml/study/nobook-phy.json")}
+        return v(a,b);
+    }
+})();
