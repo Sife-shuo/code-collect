d6572847e2feaa68ccfdc6d3c334ff72bc8e4d8e
Sife-shuo <88375951+Sife-shuo@users.noreply.github.com>
2023-07-27 12:05:15 +0800

    Add $h method

--- dom.js
+++ dom.js
@@ -10,3 +10,56 @@ Element.prototype.animate=function(params,speed){
     this.style.transition = 'all ' + speed;
     Object.keys(params).forEach((key)=>{el.style[key]=params[key]});
 }
+
+
+var $h_obj = (x) => Object.prototype.toString.call(x).slice(8, 14) === "Object";
+
+function $h() {
+    var main = document.createElement(arguments[0]);
+    child = [];
+    if (arguments[1]) {
+        if ($h_obj(arguments[1])) {
+            for (var i in arguments[1]) {
+                if (i in HTMLElement.prototype) {
+                    if ($h_obj(arguments[1][i])) {
+                        for (var k in arguments[1][i]) {
+                            main[i][k] = arguments[1][i][k]
+                        }
+                    } else {
+                        main[i] = arguments[1][i]
+                    }
+                } else {
+                    main.setAttribute(i, arguments[1][i])
+                }
+            }
+        } else {
+            child.push(arguments[1])
+        }
+        if (arguments[2]) {
+            child.push(arguments[2])
+        }
+    }
+    if (child.length) {
+        child = $h_flat(child);
+        for (var i in child) {
+            if (Object.prototype.toString.call(child[i].__proto__.__proto__.__proto__).slice(8, 15) === "Element") {
+                main.append(child[i])
+            } else {
+                main.innerText = child[i]
+            }
+        }
+    }
+    return main;
+}
+
+function $h_flat(arr) {
+    var t = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] instanceof Array) {
+            t = t.concat($h_flat(arr[i]))
+        } else {
+            t.push(arr[i])
+        }
+    }
+    return t
+}
